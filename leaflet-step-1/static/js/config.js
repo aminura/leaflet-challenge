4e3d890d18e86f6d68e6b9278eb86bcd5a0332f8
Arunima Menon <arunima_menon@Arunimas-MacBook-Air.local>
2019-10-15 14:27:44 -0700

    gitignore

--- leaflet-step-1/static/js/config.js
+++ leaflet-step-1/static/js/config.js
@@ -1,3 +1,2 @@
-/ API key
-API_KEY = "/ API key
-const API_KEY = "pk.eyJ1IjoiYXJ1bmltYW1lbm9uIiwiYSI6ImNrMHR0YnIxdjBoMm8zY2xhbGVmMmVoamwifQ.beA6Mvw6Ry5vEYehghSLxw";
+// API key
+API_KEY = "pk.eyJ1IjoiYXJ1bmltYW1lbm9uIiwiYSI6ImNrMHR0YnIxdjBoMm8zY2xhbGVmMmVoamwifQ.beA6Mvw6Ry5vEYehghSLxw";
